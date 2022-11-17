7d6cd58e1a08ba4d1c8cc1051988590c50bc523c
nisingh <nisingh@egnyte.com>
2022-11-17 09:39:16 +0530

    UT for account and random utils functions

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -1,7 +1,30 @@
 package db
 
-import "testing"
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCreateAccount(t *testing.T) {
+
+	args := CreateAccountParams{
+		Owner:    "tommy",
+		Balance:  1000,
+		Currency: "USD",
+	}
+
+	account, err := testQueries.CreateAccount(context.Background(), args)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account)
+
+	require.Equal(t, args.Owner, account.Owner)
+	require.Equal(t, args.Balance, account.Balance)
+	require.Equal(t, args.Currency, account.Currency)
+
+	require.NotEmpty(t, account.ID)
+	require.NotEmpty(t, account.CreatedAt)
 
 }
