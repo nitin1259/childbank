b40ae2100c24f43129db787cfbc9901726cd9bd6
nisingh <nisingh@egnyte.com>
2022-11-22 19:06:01 +0530

    Mock DB for testing HTTP API in Go and achieve 100% coverage

--- api/server.go
+++ api/server.go
@@ -7,12 +7,12 @@ import (
 
 // Server serves all http request of our banking service
 type Server struct {
-	store  *db.Store
+	store  db.Store
 	router *gin.Engine
 }
 
 //NewServer creates a new http server and setup routing
-func NewServer(store *db.Store) *Server {
+func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
