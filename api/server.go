6bcd0f30b82fb15983466ababfcd6575cda3b221
nisingh <nisingh@egnyte.com>
2022-11-26 16:01:12 +0530

    Implement login user API that returns PASETO or JWT access token in Go

--- api/server.go
+++ api/server.go
@@ -5,32 +5,49 @@ import (
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/nitin1259/childbank/db/sqlc"
+	"github.com/nitin1259/childbank/token"
+	"github.com/nitin1259/childbank/util"
 )
 
 // Server serves all http request of our banking service
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	store      db.Store
+	router     *gin.Engine
+	tokenMaker token.Maker
+	config     util.Config
 }
 
 //NewServer creates a new http server and setup routing
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, err
+	}
+	server := &Server{store: store, tokenMaker: tokenMaker, config: config}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
+
+	server.setUpRouter()
+
+	return server, err
+}
+
+func (server *Server) setUpRouter() {
+
+	router := gin.Default()
+
 	// add routes to router
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
-
-	return server
 }
 
 // Start runs the http server on a specific address
